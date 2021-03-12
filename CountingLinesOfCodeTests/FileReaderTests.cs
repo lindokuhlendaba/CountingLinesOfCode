ea4929721559ecd4ec8716ea52ef8ea9559f1ccd
Jayd Page <jayd.page@chillisoft.co.za>
2021-03-12 15:29:29 +0200

    Push broken FileReaderTests

---
+++ CountingLinesOfCodeTests/FileReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CountingLinesOfCodeTests
+{
+    public class FileReaderTests
+    {
+        public class Read
+        {
+            [Test]
+            public void GivenEmptyFile_ShouldReturnEmptyContent()
+            {
+                //arrange
+                var sut = new FileReader();
+                var filePath = "";
+                //act
+                var content = sut.Read(filePath);
+                //assert
+                content.Should().Be(string.Empty);
+            }
+        }
+    }
+
+    public class FileReader
+    {
+        public string Read(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
