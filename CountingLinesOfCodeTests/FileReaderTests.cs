5007ee7e551626fdeb68821254180c4bb3f6cc02
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2021-03-19 14:52:35 +0200

    add implementation for file reader

--- CountingLinesOfCodeTests/FileReaderTests.cs
+++ CountingLinesOfCodeTests/FileReaderTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace CountingLinesOfCodeTests
 {
+    [TestFixture]
     public class FileReaderTests
     {
         public class Read
@@ -13,12 +15,46 @@ namespace CountingLinesOfCodeTests
             {
                 //arrange
                 var sut = new FileReader();
-                var filePath = "";
+                var filePath = GetFilePath("FileWithoutContents.cs");
                 //act
                 var content = sut.Read(filePath);
                 //assert
                 content.Should().Be(string.Empty);
             }
+
+
+            [Test]
+            public void GivenFileNotFound_ShouldThrowAFileNotFoundException()
+            {
+                //arrange
+                var sut = new FileReader();
+                var filePath = GetFilePath("123.txt");
+                const string expectedException = "File not found";
+                //act
+                var exception = Assert.Throws<FileNotFoundException>(() => sut.Read(filePath));
+                //assert
+                exception.Message.Should().Be(expectedException);
+                exception.FileName.Should().Be(filePath);
+            }
+
+            [Test]
+            public void GivenFileWith1Line_ShouldReturnTheLineContent()
+            {
+                //arrange
+                var sut = new FileReader();
+                var filePath = GetFilePath("FileWith1Line.cs");
+                const string expected = "interface IFile { }";
+                //act
+                var content = sut.Read(filePath);
+                //assert
+                content.Should().Be(expected);
+            }
+
+            private static string GetFilePath(string fileName)
+            {
+                return Path.Combine(Directory.GetCurrentDirectory(), $@"files\{fileName}");
+            }
+
         }
     }
 
@@ -26,7 +62,13 @@ namespace CountingLinesOfCodeTests
     {
         public string Read(string filePath)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File not found", filePath);
+            }
+
+            var allText = File.ReadAllText(filePath);
+            return allText;
         }
     }
 }
