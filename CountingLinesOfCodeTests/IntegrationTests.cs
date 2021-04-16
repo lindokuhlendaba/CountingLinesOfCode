223ba0e92028d015eea283863c08b9fcb639ffd4
nishka <nishka.maharaj@chillisoft.co.za>
2021-04-16 15:48:17 +0200

    add IntegrationTest for Multiple multiline comments

--- CountingLinesOfCodeTests/IntegrationTests.cs
+++ CountingLinesOfCodeTests/IntegrationTests.cs
@@ -22,6 +22,20 @@ namespace CountingLinesOfCodeTests
             result.Should().Be(11);
         }
 
+        [Test]
+        public void GivenFileWithMultipleMultiLineCommentImplementation_ShouldReturnCorrectLineCount()
+        {
+            //Arrange
+            var fileReader = new FileReader();
+            var codeLineCounter = new CodeLineCounter();
+            var filePath = GetFilePath("FileWithMultipleLineComments.txt");
+            //Act
+            var content = fileReader.Read(filePath);
+            var result = codeLineCounter.CountLinesOfCode(content);
+            //Assert
+            result.Should().Be(19);
+        }
+
         private static string GetFilePath(string fileName)
         {
             return Path.Combine(Directory.GetCurrentDirectory(), $@"files\{fileName}");
